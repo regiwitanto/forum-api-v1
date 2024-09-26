63ee27132c755fb842bc7c05cfe241075f89fc1a
Regi Witanto <witantoregi@gmail.com>
2024-09-26 14:00:49 +0700

    create threads handler

---
+++ src/Interfaces/http/api/threads/handler.js
@@ -0,0 +1,27 @@
+const AddThreadUseCase = require('../../../../Applications/use_case/threads/AddThreadUseCase');
+
+class ThreadHandler {
+  constructor(container) {
+    this._containter = container;
+    this.postThreadHandler = this.postThreadHandler.bind(this);
+    this.getThreadDetailsHandler = this.getThreadDetailsHandler.bind(this);
+  }
+
+  async postThreadHandler(request, h) {
+    const addThreadUseCase = this._containter.getInstance(
+      AddThreadUseCase.name
+    );
+    const { id: userId } = request.auth.credentials;
+    const addedThread = await addThreadUseCase.execute(request.payload, userId);
+    const response = h.response({
+      status: 'success',
+      data: {
+        addedThread,
+      },
+    });
+    response.code(201);
+    return response;
+  }
+}
+
+module.exports = ThreadHandler;
