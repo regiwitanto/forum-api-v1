8a759613e5f6170dec7a68781ba4c0a55c3f02ad
Regi Witanto <witantoregi@gmail.com>
2024-09-26 14:18:17 +0700

    create add thread use case test

--- src/Applications/use_case/threads/_test/AddThreadUseCase.test.js
+++ src/Applications/use_case/threads/_test/AddThreadUseCase.test.js
@@ -3,14 +3,7 @@ const AddedThread = require('../../../../Domains/threads/entities/AddedThread');
 const AddThreadUseCase = require('../AddThreadUseCase');
 
 describe('AddThreadUseCase', () => {
-  /**
-   * Testing if the thread use case can orchestra step by step the
-   * for adding new thread correctly
-   */
-
   it('should orchestrating the add thread action correctly', async () => {
-    // Arrange
-
     const useCasePayload = {
       title: 'Title for thread',
       body: 'This is body for thread',
@@ -26,27 +19,21 @@ describe('AddThreadUseCase', () => {
       owner: 'user-123',
     });
 
-    /** creating dependency of use case*/
-
     const mockThreadRepository = new ThreadRepository();
 
-    /** mocking needed function */
     mockThreadRepository.addNewThread = jest
       .fn()
       .mockImplementation(() => Promise.resolve(mockAddedThread));
 
-    /** creating use case instance */
     const getThreadUsecase = new AddThreadUseCase({
       threadRepository: mockThreadRepository,
     });
 
-    //Action
     const addedThread = await getThreadUsecase.execute(
       useCasePayload,
       useCaseCredential
     );
 
-    //Assert
     expect(addedThread).toStrictEqual(
       new AddedThread({
         id: 'thread-123',
