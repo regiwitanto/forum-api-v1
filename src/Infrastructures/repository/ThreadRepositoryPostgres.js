cfc3ab967a13b9ac82cd157f69e3924fd072d43c
Regi Witanto <witantoregi@gmail.com>
2024-09-26 21:32:14 +0700

    create thread repository postgres

---
+++ src/Infrastructures/repository/ThreadRepositoryPostgres.js
@@ -0,0 +1,32 @@
+const NotFoundError = require('../../Commons/exceptions/NotFoundError');
+const ThreadRepository = require('../../Domains/threads/ThreadRepository');
+const AddedThread = require('../../Domains/threads/entities/AddedThread');
+
+class ThreadRepositoryPostgres extends ThreadRepository {
+  constructor(pool, idGenerator) {
+    super();
+    this._pool = pool;
+    this._idGenerator = idGenerator;
+  }
+
+  async addNewThread(thread, ownerId) {
+    const { title, body } = thread;
+    const id = `thread-${this._idGenerator()}`;
+    const time = new Date();
+
+    const query = {
+      text: 'INSERT INTO threads VALUES($1, $2, $3, $4, $5) Returning id, title, user_id',
+      values: [id, title, body, time, ownerId],
+    };
+
+    const result = await this._pool.query(query);
+
+    return new AddedThread({
+      id: result.rows[0].id,
+      title: result.rows[0].title,
+      owner: result.rows[0].user_id,
+    });
+  }
+}
+
+module.exports = ThreadRepositoryPostgres;
