d8294f123650b37566f461f6e759bc56168c6728
Regi Witanto <witantoregi@gmail.com>
2024-09-27 06:45:48 +0700

    add delete commenthttp test

--- src/Infrastructures/http/_test/comments.test.js
+++ src/Infrastructures/http/_test/comments.test.js
@@ -90,4 +90,90 @@ describe('/threads/{threadId}/comments endpoint', () => {
       expect(responseJson.data.addedComment).toBeDefined();
     });
   });
+
+  describe('when delete /comments/{commentId}', () => {
+    it('should response 200 with status success', async () => {
+      const server = await createServer(container);
+      const commentPayload = {
+        content: 'This is comment',
+      };
+
+      await injection(server, addUserOption(userPayload));
+
+      const auth = await injection(server, addAuthOption(loginPayload));
+
+      const {
+        data: { accessToken },
+      } = JSON.parse(auth.payload);
+
+      const thread = await injection(
+        server,
+        addThreadOption(threadPayload, accessToken)
+      );
+
+      const threadId = JSON.parse(thread.payload).data.addedThread.id;
+
+      const commentAdded = await injection(
+        server,
+        addCommentOption(commentPayload, accessToken, threadId)
+      );
+
+      const commentId = JSON.parse(commentAdded.payload).data.addedComment.id;
+
+      const response = await server.inject({
+        method: 'DELETE',
+        url: `/threads/${threadId}/comments/${commentId}`,
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(200);
+      expect(responseJson.status).toEqual('success');
+    });
+
+    it('should throw 403 if user not the owner', async () => {
+      const server = await createServer(container);
+      const commentPayload = {
+        content: 'This is comment',
+      };
+
+      await injection(server, addUserOption(userPayload));
+      await injection(server, addUserOption(notOwnerPayload));
+
+      const authOwner = await injection(server, addAuthOption(loginPayload));
+      const authNotOwner = await injection(
+        server,
+        addAuthOption(notOwnerLoginPayload)
+      );
+
+      const ownerToken = JSON.parse(authOwner.payload).data.accessToken;
+      const notOwnerToken = JSON.parse(authNotOwner.payload).data.accessToken;
+
+      const thread = await injection(
+        server,
+        addThreadOption(threadPayload, ownerToken)
+      );
+      const threadId = JSON.parse(thread.payload).data.addedThread.id;
+
+      const commentAdded = await injection(
+        server,
+        addCommentOption(commentPayload, ownerToken, threadId)
+      );
+      const commentId = JSON.parse(commentAdded.payload).data.addedComment.id;
+
+      const response = await server.inject({
+        method: 'DELETE',
+        url: `/threads/${threadId}/comments/${commentId}`,
+        headers: {
+          Authorization: `Bearer ${notOwnerToken}`,
+        },
+      });
+
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(403);
+      expect(responseJson.status).toEqual('fail');
+    });
+  });
 });
