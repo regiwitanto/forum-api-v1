4de3374de3712f30210b92e0e0bba0d34e38c91d
Regi Witanto <witantoregi@gmail.com>
2024-09-26 14:19:33 +0700

    create add thread use case

---
+++ src/Applications/use_case/threads/AddThreadUseCase.js
@@ -0,0 +1,18 @@
+const NewThread = require('../../../Domains/threads/entities/NewThread');
+
+class AddThreadUseCase {
+  constructor({ threadRepository }) {
+    this._threadRepository = threadRepository;
+  }
+
+  async execute(useCasePayload, useCaseCredential) {
+    const newThread = new NewThread(useCasePayload);
+
+    return await this._threadRepository.addNewThread(
+      newThread,
+      useCaseCredential
+    );
+  }
+}
+
+module.exports = AddThreadUseCase;
